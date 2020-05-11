b1231118956adf76ac8c31417f40def0eccce717
David <dav4thevid@gmail.com>
2020-05-11 12:25:11 +0100

    completed about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faFighterJet} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+faFighterJet = faFighterJet;
   constructor() { }
 
   ngOnInit() {
