51de0e69cad9c96562330a17d8ceb5d4b09e6dfe
David <dav4thevid@gmail.com>
2020-05-13 01:22:58 +0100

    enhan:added firebase to tools

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,10 +7,35 @@ import {faFighterJet} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-faFighterJet = faFighterJet;
+
+  showAllPage = true;
+  showPageJavascript = false;
+  showPageAngular = false;
   constructor() { }
 
   ngOnInit() {
   }
+
+  showPageAll() {
+    this.showAllPage = true;
+    this.showPageJavascript = false;
+    this.showPageAngular = false;
+  }
+
+  showJavascriptPage() {
+    this.showPageJavascript = true;
+    this.showAllPage = false;
+    this.showPageAngular = false;
+  }
+
+  showAngularPage() {
+    this.showPageJavascript = false;
+    this.showAllPage = false;
+    this.showPageAngular = true;
+  }
+
+
+
+
 
 }
