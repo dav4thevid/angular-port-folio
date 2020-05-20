e852a7c198a46aba5724345814da2fdc02367bc1
David <dav4thevid@gmail.com>
2020-05-20 15:13:22 +0100

    enhan:made some changes to navbar

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,9 +8,61 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomePageComponent implements OnInit {
   faArrowRight = faArrowRight;
+  scroll = true;
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  @HostListener('window:scroll', ['$event'])
+
+onWindowScroll(e) {
+    let element = document.querySelector('.navbar');
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbar2');
+    } else {
+      element.classList.remove('navbar2');
+    }
+  }
+
+
+
+
+//handle all page scrolling
+  // @HostListener('window:scroll', ['$event'])
+  //   scrollHandler(event) {
+  //     // console.debug("Scroll Event");
+  //     // console.log("i am scrolling");
+  //     if (event) {
+  //         this.scroll = false;
+  //         console.log('i am scrolling');
+  //     } else {
+  //       this.scroll =  true;
+  //     }
+  //   }
+
+//handle only div scrolling
+    // @HostListener('scroll', ['$event'])
+    //     scrollHandler(event) {
+    //       console.debug("Scroll Event");
+    //     }
+
+
+
+
+
+//   myNav = document.getElementById('mynav');
+//   window.onscroll = function () {
+//     "use strict";
+//     if (document.body.scrollTop >= 200 ) {
+//         myNav.classList.add("nav-colored");
+//         myNav.classList.remove("nav-transparent");
+//     }
+//     else {
+//         myNav.classList.add("nav-transparent");
+//         myNav.classList.remove("nav-colored");
+//     }
+// };
 
 }
