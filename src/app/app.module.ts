e520c6bdf4917e64908ee76fb14d4625576a8219
David <dav4thevid@gmail.com>
2020-06-30 22:44:54 +0100

    enhan:added npm-animate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AboutComponent } from './about/about.component';
 
+import { NavbarComponent } from './navbar/navbar.component';
 import { ModalComponent } from './about/modal/modal.component';
 import { BlogsComponent } from './blogs/blogs.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     FontAwesomeModule,
-    // NgbModule.forRoot(),
+    BrowserModule,
+    BrowserAnimationsModule,
     NgbModule,
     BrowserModule,
     AppRoutingModule
